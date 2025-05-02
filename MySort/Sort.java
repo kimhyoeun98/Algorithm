50230e34f76bf4e81853bdd20f6e682fe39fea33
kimhyoeun98 <wldusiu@gmail.com>
2025-05-02 14:10:59 +0900

    Add files via upload

---
+++ MySort/Sort.java
@@ -0,0 +1,26 @@
+package Sort.MySort;
+
+import java.util.Arrays;
+
+public abstract class Sort {
+	int[] orgData;
+	int[] sortedData;
+	
+	public abstract void sort();
+	
+	
+	public void setData(int[] numList) {
+		this.orgData = numList;
+		this.sortedData = numList.clone(); //deep copy 방식 
+		                                    //this.sortedData = dataList; <- 이렇게 했을 경우 shallow copy: 얕은 복사(주소만 복사)                                     
+	}
+	
+	public String getOrgData() {
+		return Arrays.toString(orgData);
+	}
+	
+	public String getSortedData() {
+		return Arrays.toString(sortedData);
+	}
+
+}
